2008ad3d05a6711374229ca644793eea9bad1a32
221RDB004 Sandis Bērziņš <125198711+SandisBerzins221RDB004@users.noreply.github.com>
2023-03-20 17:52:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 # python3
 
-def sift_down(i, data, swaps):
-    min_index = i
-    left_child = 2 * i + 1
-    right_child = 2 * i + 2
-    n = len(data)
-    if left_child < n and data[left_child] < data[min_index]:
-        min_index = left_child
-    if right_child < n and data[right_child] < data[min_index]:
-        min_index = right_child
-    if i != min_index:
-        swaps.append((i, min_index))
-        data[i], data[min_index] = data[min_index], data[i]
-        sift_down(min_index, data, swaps)
+def parallel_processing(n, m, data):
+  output = []
+threads = [(i,0) for i in range(n)]
 
-def build_heap(data):
-    swaps = []
-    n = len(data)
-    for i in range(n//2, -1, -1):
-        sift_down(i, data, swaps)
-    return swaps
-
-def main():
-    n = int(input())
-    data = list(map(int, input().split()))
-    swaps = build_heap(data)
-    m = len(swaps)
-    print(m)
-    for i, j in swaps:
-        print(i, j)
-
-if __name__ == "__main__":
+ for j in range(m):
+        thread = 0
+        for i in range(0, n):
+            if threads[i][1] < threads[thread][1]:
+                thread = i
+        
+        index, time = threads[thread]
+        output.append(threads[thread])
+        threads[thread] = (index, time + data[j])
+        
+        return output
+    
+    def main():
+        
+        first_line = input().split()
+        n = int(first_line[0])
+        m = int(first_line[1])
+        data = list(map(int, input().split()))
+        result = parallel_processing(n, m, data)
+        
+        for index,time in result:
+            print(index, time)
+        
+        
+    if __name__ == "__main__":
     main()
+    
